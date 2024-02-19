5036aa6d025d78193e72d408de0f7d74929a1f14
albysukma <144236729+albysukma@users.noreply.github.com>
2024-02-19 15:25:46 +0700

    Update data_mahasiswa.php
    update data semester

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h1>Data Mahasiswa</h1>";
-echo "MIM : 22110052<bt>";
-echo "Nama Lengkap : M.Alby Sukma";
-echo "Kelas : 22 MI B";``````
+echo "MIM : 22110052<br>";
+echo "Nama Lengkap : M.Alby Sukma<br>";
+echo "Kelas : 22 MI B";
+echo "Semester Genap";
 ?>
